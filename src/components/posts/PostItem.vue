163bdce457cc336b24e7d42888ab498dbacaf2ab
suyeon <iamsxx93@gmail.com>
2024-09-18 23:09:07 +0900

    이전으로 수정

--- src/components/posts/PostItem.vue
+++ src/components/posts/PostItem.vue
@@ -35,7 +35,7 @@ defineEmits(['modal']);
   overflow: hidden;
   text-overflow: ellipsis;
   display: -webkit-box;
-  -webkit-line-clamp: 3;
+  -webkit-line-clamp: 2;
   -webkit-box-orient: vertical;
 }
 </style>
